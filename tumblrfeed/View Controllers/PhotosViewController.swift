d18225e470b4cdb1f834372007be7c3d504344dd
Christopher Guan <cwguan@ucsd.edu>
2018-01-31 01:19:25 -0800

    Finished Milestone 3, Step 1; Need to clarify creating table view outlet

--- tumblrfeed/View Controllers/PhotosViewController.swift
+++ tumblrfeed/View Controllers/PhotosViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var posts: [[String: Any]] = []
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
@@ -33,10 +37,22 @@ class PhotosViewController: UIViewController {
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
                 //Store the returned array of dictionaries in our posts property
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
+                
                 // TODO: Reload the table view
             }
         }
         task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
